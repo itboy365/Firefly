59180984f1c475775a081fd22f296c0e6f2ad069
itboy <admin@itboy.top>
2026-09-12 20:22:01 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,31 +6,23 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/my-avatar.jpg",
 
 	// 名字
-	name: "XiaYe",
+	name: "权芯365",
 
 	// 个人签名
-	bio: "Hello, I'm XiaYe.",
+	bio: "一个90后保定小伙，爱好瞎折腾。",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
-	// 访问https://icones.js.org/ 获取图标代码，
-	// 如果想使用尚未包含相应的图标集，则需要安装它
-	// `pnpm add @iconify-json/<icon-set-name>`
+	// 访问 https://icones.js.org/ 获取图标代码
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-			name: "GitHub",
-			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
-			showName: false,
-		},
-		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:me@jiaquan.top",
 			showName: false,
 		},
 		{
