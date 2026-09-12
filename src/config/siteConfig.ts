fc0950166c96704eb887ccaafde14fe3fca3923a
itboy <admin@itboy.top>
2026-09-12 20:45:26 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -42,7 +42,7 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "权芯365的小基地",
+	title: "权芯365",
 
 	// 站点副标题
 	subtitle: "说点想说的，写点想写的小心思",
@@ -117,7 +117,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "权芯365的小基地",
+		title: "权芯365",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
